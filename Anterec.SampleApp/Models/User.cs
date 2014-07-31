e5feb370e76000ef8d939c5621087fbd80eaaeab
brentj73 <brent@anterec.co.uk>
2014-07-31 16:04:24 +0100

    Added example of re-routed POST action

---
+++ Anterec.SampleApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Anterec.SampleApp.Models
+{
+    /// <summary>
+    /// The user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+    }
+}
