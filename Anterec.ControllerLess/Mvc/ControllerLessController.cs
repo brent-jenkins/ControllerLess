be6f619c12d2fefd1bbe9922ec6342e8965d4f0a
brentj73 <brent@anterec.co.uk>
2014-08-14 10:51:51 +0100

    Added x-controller handling to view controller

--- Anterec.ControllerLess/Mvc/ControllerLessController.cs
+++ Anterec.ControllerLess/Mvc/ControllerLessController.cs
@@ -14,7 +14,9 @@
         public ActionResult Index()
         {
             var action = RouteData.Values["x-action"].ToString();
+            var controller = RouteData.Values["x-controller"].ToString();
             RouteData.Values["action"] = action;
+            RouteData.Values["controller"] = controller;
             if (RouteData.Values["area"] != null)
             {
                 RouteData.DataTokens["area"] = RouteData.Values["area"].ToString();
