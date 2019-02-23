23d16488187cbf62e83266ff9a206254a62b9a08
Brent Jenkins <brent@anterec.co.uk>
2019-02-23 10:25:09 +0000

    Updated javascript libraries in sample project to latest versions

--- Anterec.SampleApp/Scripts/_references.js
+++ Anterec.SampleApp/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <autosync enabled="true" />
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <autosync enabled="true" />
 /// <reference path="bootstrap.min.js" />
-/// <reference path="jquery-2.2.2.js" />
 /// <reference path="jquery-2.2.2.min.js" />
