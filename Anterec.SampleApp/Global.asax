34fcde931e16e1b679c275751796e8094f769b8a
Brent Jenkins <6659947+brent-jenkins@users.noreply.github.com>
2023-07-25 21:54:27 +0100

    Removed sample app and updated Git URL for NuGet (#9)
    I was alerted that the Sample App that was included with the solution had security issues with its dependencies. The simplest fix for this is to remove the Sample App. While it was useful to see how the ControllerLess library worked, it isn't essential to the solution and as time goes by, it would need further maintenance and potential redevelopment. As the Sample App was not the main focus of the solution, it has now been removed.

--- Anterec.SampleApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Anterec.SampleApp.MvcApplication" Language="C#" %>
--- Anterec.SampleApp/Scripts/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="jquery-3.3.1.js" />
-/// <autosync enabled="true" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="jquery-2.2.2.min.js" />
